c2c2020430fc3802462d82ae1b33e8648e593460
hreddyCMU <92760673+hreddyCMU@users.noreply.github.com>
2022-04-18 18:41:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const { Kafka } = require("kafkajs");
 run().then(() => console.log("Done"), err => console.log(err));
 
 async function run() {
-  const kafka = new Kafka({ brokers: ["localhost:9092"] });
+    const kafka = new Kafka({
+        clientId: 'my-app',
+        brokers: ['b-2.customer.1awnnt.c6.kafka.us-east-2.amazonaws.com:9092', 'b-1.customer.1awnnt.c6.kafka.us-east-2.amazonaws.com:9092','b-3.customer.1awnnt.c6.kafka.us-east-2.amazonaws.com:9092'],
+        ssl: false
+      })
+    
 
   const consumer = kafka.consumer({ groupId: "" + Date.now() });
 
-  await consumer.connect();
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'MSKTutorialTopic', fromBeginning: true })
 
-  await consumer.subscribe({ topic: "quickstart-events", fromBeginning: true });
-  await consumer.run({ 
-    eachMessage: async (data) => {
-      console.log(data);
-    }
-  });
+  await consumer.run({
+   eachMessage: async ({ topic, partition, message }) => {
+     console.log({
+       value: message
+     })
+   },
+ });
 
 }
